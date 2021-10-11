0a76d430956ee521ffcac4d90bd137dd40c6d42c
JuanCarlos3 <hwankim2k@gmail.com>
2021-10-10 17:21:02 -0700

    added senators

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-ra_9%6^f+nnac(vn6+huqdk#o8#)ls1fms@+t6)(0_)x%ea9_q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'myapp'
 ]
 
 MIDDLEWARE = [
--- restapi/restapi/urls.py
+++ restapi/restapi/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('Senator', views.add_senator),
+    path('<str:Senator_name>', views.get_senator),
 ]
+
